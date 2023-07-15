a26aa7a22a95d7519b972393b85b99fb3720287f
Sr. Galán <antoniogalanG15@gmail.com>
2023-07-16 01:44:48 +0200

    Archivos de la parte bonus inicializados
    Se duplicaron los archivos de la parte obligatoria cambiando aquellos
    elementos del código que eran necesarios para la compatibilidad.

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antgalan <antgalan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 10:58:34 by antgalan          #+#    #+#             */
+/*   Updated: 2023/07/16 01:39:41 by antgalan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if (!str)
+		return (NULL);
+	while (*str != (char) c)
+	{
+		if (!*str++)
+			return (NULL);
+	}
+	return ((char *) str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *) malloc(len + 1);
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, ft_strlen(s1));
+	ft_memcpy(str + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	return (str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
